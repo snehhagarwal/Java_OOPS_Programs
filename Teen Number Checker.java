67971fd9e80597657d6e8a251550217ed8f5e382
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2024-03-25 17:58:33 +0530

    Update Teen Number Checker.java

--- Teen Number Checker.java
+++ Teen Number Checker.java
@@ -1,13 +1,9 @@
 /*We'll say that a number is "teen" if it is in the range 13 -19 (inclusive).
-
 Write a method named hasTeen with 3 parameters of type int.
-
 The method should return boolean and it needs to return true if one of the parameters is in range 13(inclusive) - 19 (inclusive). Otherwise return false.
 Write another method named isTeen with 1 parameter of type int.
-
 The method should return boolean and it needs to return true if the parameter is in range 13(inclusive) - 19 (inclusive). Otherwise return false.*/
 Code-->
-
 public class TeenNumberChecker {
     public static boolean hasTeen(int first,int second,int third){
         if((first>=13 && first<=19) || (second>=13 && second<=19) || (third>=13 && third<=19)){
